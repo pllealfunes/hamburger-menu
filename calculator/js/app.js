a593c5cf135c88581a475846e8e434117030bf80
Paula Leal <paula61395@gmail.com>
2021-03-15 20:41:14 -0400

    able to do multiple operations

--- calculator/js/app.js
+++ calculator/js/app.js
@@ -8,20 +8,23 @@ let operator = '';
 let equation;
 buttons.forEach(function (btn) {
     btn.addEventListener('click', function () {
-        console.log(Number('20%'));
         if (isNaN(Number(btn.value)) && operator === '') {
             operator += btn.value;
-            //input.value += operator;
+            input.value += operator;
         } else if (operator === '') {
             firstNum += btn.value;
             //input.value += firstNum;
         } else if (firstNum != '' && operator != '' && Number(btn.value) || btn.value === '0') {
             secondNum += btn.value;
-            //input.value += secondNum;
+            input.value += secondNum;
         } else if (btn.value === '=' && firstNum != '' && secondNum != '') {
             findAnswer();
             secondNum = '';
             operator = '';
+        } else if (isNaN(Number(btn.value)) && operator != '') {
+            findAnswer();
+            secondNum = '';
+            operator = btn.value
         } else {
             console.log('try again');
         }
