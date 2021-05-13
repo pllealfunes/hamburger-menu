7338e8eafa353355d59ce63779f12eaf1300937b
Paula Leal <paula61395@gmail.com>
2021-05-13 14:55:33 -0400

    can now create drawing and added clear button

--- etch-sketch/README.md
+++ etch-sketch/README.md
@@ -3,7 +3,9 @@
 I am working on a etch-a-sketch project! This idea comes from the Odin Project.
 
 ## To Do
-* Create 16X16grid
-* Change background color on hover
-* Keep color after mouse moves away
+* Create 16X16grid ☑️
+* Change background color on hover ☑️
+* Keep color after mouse moves away ☑️
+* Add clear button ☑️
 * Add ability to create different grid sizes
+* Add rainbow option
--- etch-sketch/app.js
+++ etch-sketch/app.js
@@ -1,10 +1,32 @@
-let numDivs;
+//function genereateGrid() {
+//let numDivs = document.querySelector('#block-number').value;
+document.querySelector('#new-grid').addEventListener('click', event => {
+    if (document.querySelector('#container').length > 0) {
+        //document.querySelector('.block').parentNode.removeChild('.block');
+        console.log('hi');
+    } else {
+        let numDivs = document.querySelector('#block-number').value;
+        for (let number = 0; number < numDivs; number++) {
+            let column = document.createElement('div');
+            document.querySelector('#container').appendChild(column);
+            for (let i = 0; i < numDivs; i++) {
+                let block = document.createElement('div');
+                block.classList.add('block');
+                block.style.border = '1px solid';
+                block.style.borderColor = 'pink';
+                block.style.width = '30px';
+                block.style.height = '30px';
+                column.appendChild(block);
+            }
+        }
+    }
 
-
-for (let number = 0; number < 16; number++) {
+})
+//genereateGrid();
+/*for (let number = 0; number < numDivs; number++) {
     let column = document.createElement('div');
     document.querySelector('#container').appendChild(column);
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < numDivs; i++) {
         let block = document.createElement('div');
         block.classList.add('block');
         block.style.border = '1px solid';
@@ -13,14 +35,18 @@ for (let number = 0; number < 16; number++) {
         block.style.height = '30px';
         column.appendChild(block);
     }
-}
-
-document.querySelector('.block').addEventListener("mouseover", mouseOver);
-
-function mouseOver() {
-    document.querySelector('.block').style.backgroundColor = "red";
-}
-
-/*function mouseOut() {
-    document.getElementById("demo").style.color = "black";
 }*/
+//}
+
+document.querySelectorAll('.block').forEach(block => {
+    block.addEventListener('mouseover', event => {
+        block.style.backgroundColor = "red";
+    })
+})
+
+document.querySelector('#clear-board').addEventListener('click', event => {
+    document.querySelectorAll('.block').forEach(block => {
+        block.style.backgroundColor = "";
+    })
+})
+
