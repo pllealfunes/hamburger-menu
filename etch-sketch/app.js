219acc9d31faa3eb4df0f3b99fa936064348dedf
Paula Leal <paula61395@gmail.com>
2021-05-14 13:57:38 -0400

    can now generate new grid

--- etch-sketch/app.js
+++ etch-sketch/app.js
@@ -1,48 +1,28 @@
-//function genereateGrid() {
-//let numDivs = document.querySelector('#block-number').value;
+'use strict'
+
+generateGrid(16);
+
 document.querySelector('#new-grid').addEventListener('click', event => {
-    if (document.querySelector('#container').length > 0) {
-        //document.querySelector('.block').parentNode.removeChild('.block');
-        console.log('hi');
-    } else {
-        let numDivs = document.querySelector('#block-number').value;
-        for (let number = 0; number < numDivs; number++) {
-            let column = document.createElement('div');
-            document.querySelector('#container').appendChild(column);
-            for (let i = 0; i < numDivs; i++) {
-                let block = document.createElement('div');
-                block.classList.add('block');
-                block.style.border = '1px solid';
-                block.style.borderColor = 'pink';
-                block.style.width = '30px';
-                block.style.height = '30px';
-                column.appendChild(block);
-            }
+    document.querySelector('#container').innerHTML = '';
+    generateGrid(document.querySelector('#block-number').value);
+})
+
+function generateGrid(numDivs) {
+    for (let number = 0; number < numDivs; number++) {
+        let column = document.createElement('div');
+        document.querySelector('#container').appendChild(column);
+        for (let i = 0; i < numDivs; i++) {
+            let block = document.createElement('div');
+            block.classList.add('block');
+            column.appendChild(block);
         }
+        document.querySelectorAll('.block').forEach(block => {
+            block.addEventListener('mouseover', event => {
+                block.style.backgroundColor = "red";
+            })
+        })
     }
-
-})
-//genereateGrid();
-/*for (let number = 0; number < numDivs; number++) {
-    let column = document.createElement('div');
-    document.querySelector('#container').appendChild(column);
-    for (let i = 0; i < numDivs; i++) {
-        let block = document.createElement('div');
-        block.classList.add('block');
-        block.style.border = '1px solid';
-        block.style.borderColor = 'pink';
-        block.style.width = '30px';
-        block.style.height = '30px';
-        column.appendChild(block);
-    }
-}*/
-//}
-
-document.querySelectorAll('.block').forEach(block => {
-    block.addEventListener('mouseover', event => {
-        block.style.backgroundColor = "red";
-    })
-})
+}
 
 document.querySelector('#clear-board').addEventListener('click', event => {
     document.querySelectorAll('.block').forEach(block => {
