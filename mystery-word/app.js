bff074d1ee1d7badb2ca87bbad84521b9426c473
Paula Leal <paula61395@gmail.com>
2021-06-18 14:21:29 -0400

    can now replace dashes with letters...need to check if letter appears more than once

--- mystery-word/app.js
+++ mystery-word/app.js
@@ -20,19 +20,23 @@ const app = Vue.createApp({
         this.num = this.words[Math.floor((Math.random() * this.words.length))];
         this.mysteryWord = this.num[0];
         this.hint = this.num[1];
+        for (let number = 0; number < this.mysteryWord.length; number++) {
+            this.space += '_'
+        }
         console.log(this.mysteryWord);
     },
     methods: {
-
         userGuess() {
             if (this.mysteryWord.includes(this.guess)) {
                 let splitWord = this.space.split("");
                 console.log(splitWord)
                 let index = this.mysteryWord.indexOf(this.guess)
-                console.log(splitWord[index + 1] = this.guess)
-                //this.space = splitWord[index + 1] = this.guess
-
-                //console.log(splitWord)
+                console.log(splitWord[index] = this.guess)
+                splitWord[index] = this.guess
+                //splitWord.pop()
+                console.log(splitWord)
+                //splitWord.splice(index + 1, 1)
+                this.space = splitWord.join("")
                 //this.space = splitWord.join()
                 console.log(this.mysteryWord.indexOf(this.guess));
                 console.log("includes")
@@ -44,14 +48,7 @@ const app = Vue.createApp({
         }
     },
     computed: {
-        randomWord() {
-            //this.mysteryWord = this.words[Math.floor((Math.random() * this.words.length))];
-            //this.hint = this.mysteryWord[1]
-            for (let number = 0; number < this.mysteryWord.length; number++) {
-                this.space += ' _ '
-            }
-            return this.space
-        }
+
     }
 
 }).mount('#app');
